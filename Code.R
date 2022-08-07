5655ae03266ffb3ab74a745cd12b764d6031c1a9
Jhonattan Lino <jhonattanlino@outlook.com>
2022-08-06 21:44:57 -0300

    Update Code.R

---
+++ Code.R
@@ -0,0 +1,28 @@
+### Loading libraries
+library(dplyr)
+library(RMySQL)
+library(xts)
+library(lubridate)
+
+### Creating connction
+con <- dbConnect(RMySQL::MySQL(),
+                 dbname='retornos',
+                 host='localhost',
+                 port=3306,
+                 user='root',
+                 password='Zurique')
+
+print(dbListTables(con)) ### To see tables in data base
+
+### Loading tables
+m2 <- read.zoo(dbReadTable(con, 'm2'))
+m2<- xts(m2) ### Converting to xts
+infl <- read.zoo(dbReadTable(con, 'inflacao'))
+infl <- xts(infl)
+
+### Replace data in Inflation
+index(infl) <- lubridate::floor_date(ymd(index(infl)), 'month')
+
+### Merge data
+macro <- merge(m2, infl, join = 'left', fill = NA)
+
